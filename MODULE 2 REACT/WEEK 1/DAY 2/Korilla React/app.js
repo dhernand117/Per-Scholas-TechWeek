9efe602cfda8bc2652076e4ec88602729edb5fbc
Diego Hernandez <diegohervel@gmail.com>
2021-01-14 10:54:53 -0500

    Korilla React receipts

---
+++ MODULE 2 REACT/WEEK 1/DAY 2/Korilla React/app.js
@@ -0,0 +1,47 @@
+// console.log(receipts);
+// console.log(receipts[0].order);
+class App extends React.Component {
+    state = {
+            receipts
+    }
+    // constructor(props) {
+    // super(props);
+    // this.state = receipts;}
+
+  render () {
+    //   this.state.receipts.forEach(element => {
+    //       console.log(element);
+    //   });
+      return(
+          <div>
+              <h1 className="truck-name">Korilla</h1>
+              <div>
+                {this.state.receipts.map( (receipt) => !receipt.paid ? <Receipt receipt= {receipt}/> : "")}
+                {/* {this.state.receipts.map( (receipt) => receipt.paid ? <Receipt receipt= {receipt}/> : "")} */}
+              </div>
+          </div>
+      )
+  }
+}
+
+class Receipt extends React.Component {
+    render () {
+        console.log(this.props);
+        return (
+            <div id="receipts">
+                <h2>{this.props.receipt.person}</h2>
+                <ul id="items">
+                <li><span>Main: </span>{this.props.receipt.order.main}</li>
+                <li><span>Protein: </span>{this.props.receipt.order.protein}</li>
+                <li><span>Rice: </span>{this.props.receipt.order.rice}</li>
+                <li><span>Sauce: </span>{this.props.receipt.order.sauce}</li>
+                <li><span>Drink: </span>{this.props.receipt.order.drink}</li>
+                <li><span>Cost: </span>{this.props.receipt.order.cost}</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
